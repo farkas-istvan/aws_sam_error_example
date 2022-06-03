20d10a36432e04605dc8ced9631119ea5dc98572
Farkas István <i.farkas@upscale-labs.com>
2022-06-03 12:45:46 +0200

    init

---
+++ module_2/src/main/java/org/example/module_2/ApiGatewayRequestHandler.java
@@ -0,0 +1,40 @@
+package org.example.module_2;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.example.module_client.request.Module2Request;
+import org.example.module_core.PrinterUtil;
+
+@Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ApiGatewayRequestHandler
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    PrinterUtil printerUtil = new PrinterUtil();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent event, Context context) {
+
+        try {
+            Module2Request request = new ObjectMapper().readValue(event.getBody(), Module2Request.class);
+            printerUtil.print(request.getKey(), request.getValue());
+        } catch (JsonProcessingException e) {
+            System.out.println("An error has occurred!");
+        }
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
+        response.setBody("Everything OK");
+
+        return response;
+    }
+
+}
---
+++ module_core/src/main/java/org/example/module_core/PrinterUtil.java
@@ -0,0 +1,7 @@
+package org.example.module_core;
+
+public class PrinterUtil {
+    public void print(String key, String value) {
+        System.out.println("Key: " + key + ", value: " + value);
+    }
+}
